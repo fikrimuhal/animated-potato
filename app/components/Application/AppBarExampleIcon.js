7309102fbb30a4d477fd92c3ae17a6109b707938
glcebru <glcebru94@gmail.com>
2016-07-01 12:25:29 +0300

    ufak teşek küçük çalışmalar

---
+++ app/components/Application/AppBarExampleIcon.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const AppBarExampleIcon = () => (
+  <AppBar
+    title="Fikrimuhal Teknoloji"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />
+);
+
+export default AppBarExampleIcon;
--- app/components/Application/index.js
+++ app/components/Application/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import Header from "../Header";
 import Deneme2 from "./Deneme2";
 import Example from "./Example";
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import AppBarExampleIcon from './AppBarExampleIcon';
+import AutoCompleteExampleSimple from './AutoCompleteExampleSimple';
+import BadgeExampleSimple from './BadgeExampleSimple';
 
 /**
  * Import locally scoped styles using css-loader
@@ -9,23 +15,48 @@ import Example from "./Example";
  *
  * More info: https://github.com/webpack/css-loader#local-scope
  */
- var styles = {
-   kirmizi: {
-     backgroundColor: "#FAEBD7"
-   }
- }
-var degisken = "ebru"
+
+var style={
+  boyut:{
+    width:'500px'
+  },
+  appBar:{
+    width:'600px'
+  },
+  
 
 
-const Application = () => {
-    return <div style= {styles.kirmizi}>
+}
 
-    <Example/>
-    <Deneme2/>
+ const App = () => (
+   <div>
+      <div>
+        <MuiThemeProvider>
+          <BadgeExampleSimple/>
+        </MuiThemeProvider>
+        </div>
+
+     <div style={style.appBar}>
+      <MuiThemeProvider>
+        <AppBarExampleIcon />
+      </MuiThemeProvider>
+    </div>
+      <div style={style.boyut}>
+        <MuiThemeProvider>
+          <AutoCompleteExampleSimple/>
+          </MuiThemeProvider>
+      </div>
+
+      <div>
+        <MuiThemeProvider>
+          <MyAwesomeReactComponent />
+        </MuiThemeProvider>
+      </div>
 
     </div>
-};
 
-Application.displayName = 'Application';
+);
 
-export default Application;
+App.displayName = 'App';
+
+export default App;
