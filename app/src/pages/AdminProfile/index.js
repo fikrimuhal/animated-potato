eb8d02e0a9eb26adf9f9f7ef2901fc79aab47d23
Ebru Gulec <glcebru94@gmail.com>
2016-07-26 19:27:09 +0300

    Question Set Details Table Style

--- app/src/pages/AdminProfile/index.js
+++ app/src/pages/AdminProfile/index.js
@@ -1,68 +1,56 @@
-import React from 'react'
+import React from 'react';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
 import RaisedButton from 'material-ui/RaisedButton';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
-import TextField from 'material-ui/TextField'
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 
-const styles = {
-  customWidth: {
-   width: 150,
- },
-};
-export default class UserProfiles extends React.Component {
+
+export default class ToolbarExamplesSimple extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = {value: 1};
+    this.state = {
+      value: 3,
+    };
   }
-handleChange = (event, index, value) => this.setState({value});
-render() {
-  return (
-      <div>
-      <h3>Admin Profili</h3>
-        <br/>
-        <div>
 
-          <Table>
-            <TableHeader>
-             <TableRow>
-               <TableHeaderColumn>Ad Soyad</TableHeaderColumn>
-               <TableHeaderColumn>İşlemler</TableHeaderColumn>
-             </TableRow>
-            </TableHeader>
+  handleChange = (event, index, value) => this.setState({value});
 
-            <TableBody>
-             <TableRow>
-               <TableRowColumn>Ayşe Ak</TableRowColumn>
-               <TableRowColumn><div style={styles.button}>
-                 <RaisedButton label="Sil" secondary={true}/>
-                 <RaisedButton label="Düzenle" primary={true}/>
-               </div></TableRowColumn>
-             </TableRow>
-
-             <TableRow>
-               <TableRowColumn>Ali Yılmaz</TableRowColumn>
-               <TableRowColumn><div style={styles.button}>
-                 <RaisedButton label="Sil" secondary={true}/>
-                 <RaisedButton label="Düzenle" primary={true}/>
-               </div></TableRowColumn>
-
-             </TableRow>
-             <TableRow>
-               <TableRowColumn>Zehra Kıgız</TableRowColumn>
-               <TableRowColumn><div style={styles.button}>
-                 <RaisedButton label="Sil" secondary={true}/>
-                 <RaisedButton label="Düzenle" primary={true}/>
-               </div></TableRowColumn>
-             </TableRow>
-            </TableBody>
-            </Table>
-          </div>
-
-          <br/>
-            <div>
-              <TextField
-                hintText="Admin Ara"/>
-                <br />
-            </div>
-      </div>
-  );
-}}
+  render() {
+    return (
+      <Toolbar>
+        <ToolbarGroup firstChild={true}>
+          <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+            <MenuItem value={1} primaryText="All Broadcasts" />
+            <MenuItem value={2} primaryText="All Voice" />
+            <MenuItem value={3} primaryText="All Text" />
+            <MenuItem value={4} primaryText="Complete Voice" />
+            <MenuItem value={5} primaryText="Complete Text" />
+            <MenuItem value={6} primaryText="Active Voice" />
+            <MenuItem value={7} primaryText="Active Text" />
+          </DropDownMenu>
+        </ToolbarGroup>
+        <ToolbarGroup>
+          <ToolbarTitle text="Options" />
+          <FontIcon className="muidocs-icon-custom-sort" />
+          <ToolbarSeparator />
+          <RaisedButton label="Create Broadcast" primary={true} />
+          <IconMenu
+            iconButtonElement={
+              <IconButton touch={true}>
+                <NavigationExpandMoreIcon />
+              </IconButton>
+            }
+          >
+            <MenuItem primaryText="Download" />
+            <MenuItem primaryText="More Info" />
+          </IconMenu>
+        </ToolbarGroup>
+      </Toolbar>
+    );
+  }
+}
--- app/src/pages/QuestionSetDetails/index.js
+++ app/src/pages/QuestionSetDetails/index.js
@@ -5,6 +5,18 @@ import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import { TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import {log2,db,util} from '../../utils/'
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import MenuItem from 'material-ui/MenuItem';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 
 const {Table, Column, Cell} = FixedDataTable;
@@ -14,9 +26,13 @@ const styles = {
     backgroundColor:"#f1f1f1",
     margin: '134px'
   },
-  tableStyle: {
-    width: '100%',
-  }
+
+   buttonPadding: {
+     marginRight: '5px'
+   },
+   toolbar:{
+     width: '1000'
+   }
 }
 const setModels = [
   {
@@ -57,11 +73,15 @@ export default class QuestionSetDetails extends React.Component {
 
     this.state = {
       filteredDataList: this._dataList,
-      data2: setModels
+      data2: setModels,
+      value: 3,
+      addOptionDisplay: 'none'
     };
     this._onFilterChange = this._onFilterChange.bind(this);
   }
-
+  handleChange = function(event, index, value){
+    this.setState({value});
+}
   _onFilterChange(e) {
     if (!e.target.value) {
       this.setState({
@@ -81,6 +101,9 @@ export default class QuestionSetDetails extends React.Component {
       filteredDataList: new DataListWrapper(filteredIndexes, this._dataList),
     });
   }
+  handleSearchSet = function(event, value){
+    this.setState({addOptionDisplay: true});
+  }
   handleSetSave = function(event, value){
       var model = this.state.data2
       var input = this.refs.inputQuestionSet.input
@@ -96,48 +119,67 @@ export default class QuestionSetDetails extends React.Component {
       console.log(this.state.data2);
     db.setQuestionSetAddToStorage(input.value, util.guid())
   }
-
   render() {
     var {filteredDataList} = this.state;
 
     return (
         <div>
-          <h3>Question Set Details</h3>
-          <br />
-            <TextField onChange={this._onFilterChange}
-              hintText="Soru Seti Ara"
-            />
-          <br />
             <div>
-              <TextField ref="inputQuestionSet"
-                hintText="Soru Seti Ekle"
-              />
+
               <br/>
-              <RaisedButton label="Ekle" secondary={true} onClick={this.handleSetSave.bind(this)}/>
-              <br/>
+
+                <Toolbar style={styles.toolbar}>
+                  <ToolbarGroup>
+                    <ToolbarTitle text="Question Set Details"/>
+                    <FontIcon className="muidocs-icon-custom-sort" />
+                    <ToolbarSeparator />
+                    <IconMenu
+                      iconButtonElement={
+                        <IconButton touch={true}>
+                          <NavigationExpandMoreIcon />
+                        </IconButton>
+                      }
+                    >
+                      <MenuItem primaryText="Soru Seti Ekle" onClick={this.handleSearchSet.bind(this)}/>
+                    </IconMenu>
+
+                    <TextField onChange={this._onFilterChange}
+                      hintText="Soru Seti Ara"
+                    />
+                  </ToolbarGroup>
+
+                </Toolbar>
+
             </div>
-            <Table style={styles.tableStyle}
+            <div style={{display: this.state.addOptionDisplay}}>
+            <TextField ref="inputQuestionSet"
+              hintText="Soru Seti Ekle"
+            />
+            <br/>
+            <RaisedButton label="Ekle" secondary={true} onChange={this.handleSetSave.bind(this)}/>
+            </div>
+            <Table
               rowHeight={50}
               rowsCount={filteredDataList.getSize()}
-              headerHeight={200}
-
-              height={340}
+              headerHeight={50}
+              height={700}
+              width= {1200}
               {...this.props}>
             <Column
                 header={<Cell>Soru Seti Adı</Cell>}
                 cell={<TextCell data={filteredDataList} col="firstName" />}
                 fixed={true}
-                width={200}
+                width={400}
               />
               <Column
                 header={<Cell>Soru Sayısı</Cell>}
                 cell={<TextCell data={filteredDataList} col="lastName" />}
                 fixed={true}
-                width={200}
+                width={400}
               />
               <Column
                 header={<Cell>İşlemler</Cell>}
-                cell={<div><RaisedButton label="Sil" secondary={true}/>
+                cell={<div><RaisedButton label="Sil" secondary={true} style={styles.buttonPadding}/>
                       <RaisedButton label="Düzenle" primary={true}/></div>
                   }
                 width={400}
