f8a0022609a8d6cde8b7035b7d40ad4a40337df4
Mucahit Kantepe <mucahitkantepe@gmail.com>
2016-09-02 16:27:09 +0300

    UserController and User -> getPersonnels, getAdmins and makeAdmin, makePersonnel

--- backend/AnimatedPotatoBackend/app/controllers/UserController.scala
+++ backend/AnimatedPotatoBackend/app/controllers/UserController.scala
@@ -1,26 +1,27 @@
 package controllers
 
-import models.{Users, User}
+import models.{ID, ResponseMessage, User, Users}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
+import utils.Constants
 import utils.Formatter._
+
 /**
   * Created by who on 23.08.2016.
   */
-class UserController extends Controller{
+class UserController extends Controller {
 
 
   def insertUser() = Action { implicit request =>
     try {
       val user: User = request.body.asJson.get.as[User]
-      Users.insert(User(username = user.username,password = user.password,email = user.email))
+      Users.insert(User(username = user.username, password = user.password, email = user.email))
       Ok("1")
     }
     catch {
       case e: Exception => BadRequest(s"-1 $e")
     }
   }
-
 
   def updateUser() = Action { implicit request =>
     try {
@@ -62,5 +63,41 @@ class UserController extends Controller{
     }
   }
 
+  def getPersonnels = Action {
+    Ok(Json.toJson(Users.getPersonnelList))
+  }
 
+  def getAdmins = Action {
+    Ok(Json.toJson(Users.getAdminList))
+  }
+
+  def makeAdmin = Action { implicit request =>
+
+    request.body.asJson.flatMap(_.validate[ID].asOpt) match {
+
+      case Some(id) =>
+
+        if (Users.makeAdmin(id.id)) {
+          Ok(Json.toJson(ResponseMessage(Constants.OK, Constants.OK_MESSAGE)))
+        }
+        else InternalServerError(Json.toJson(Constants.FAIL, Constants.SERVER_ERROR_MESSAGE))
+
+      case _ => BadRequest(Json.toJson(ResponseMessage(Constants.FAIL, Constants.UNEXPECTED_ERROR_MESSAGE)))
+    }
+  }
+
+  def makePersonnel = Action { implicit request =>
+
+    request.body.asJson.flatMap(_.validate[ID].asOpt) match {
+
+      case Some(id) =>
+
+        if (Users.makePersonnel(id.id)) {
+          Ok(Json.toJson(ResponseMessage(Constants.OK, Constants.OK_MESSAGE)))
+        }
+        else InternalServerError(Json.toJson(Constants.FAIL, Constants.SERVER_ERROR_MESSAGE))
+
+      case _ => BadRequest(Json.toJson(ResponseMessage(Constants.FAIL, Constants.UNEXPECTED_ERROR_MESSAGE)))
+    }
+  }
 }
